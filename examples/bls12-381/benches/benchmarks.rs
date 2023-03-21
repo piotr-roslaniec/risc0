893c3fe64d73ac8dd045409519cc62c70924a7f8
Piotr Roslaniec <p.roslaniec@gmail.com>
2023-03-21 09:55:11 +0100

    fixed benchmarks

--- examples/bls12-381/benches/benchmarks.rs
+++ examples/bls12-381/benches/benchmarks.rs
@@ -4,22 +4,21 @@
 use bls12_381::setup_g1_addition_prover;
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 
-const BENCH_CASES: [usize; 7] = [2, 4, 8, 16, 32, 64, 128];
+const BENCH_CASES: [usize; 6] = [2, 4, 8, 16, 32, 64];
 
-pub fn bench_g1_addition(c: &mut Criterion) {
+pub fn bench_bls12_381(c: &mut Criterion) {
     let mut rng = ark_std::test_rng();
-    let mut group = c.benchmark_group("g1-addition");
+    let mut group = c.benchmark_group("bls12-381");
     group.sample_size(10);
 
     for n_g1 in BENCH_CASES.iter() {
-        let (mut prover, _expected_aggregate) = setup_g1_addition_prover(&mut rng, *n_g1);
-
-        group.bench_with_input(BenchmarkId::new("G1", n_g1), n_g1, |b, _| {
+        group.bench_function(BenchmarkId::new("g1-addition", n_g1), |b| {
+            let (mut prover, _expected_aggregate) = setup_g1_addition_prover(&mut rng, *n_g1);
             b.iter(|| prover.run().expect("Code should be provable"))
         });
     }
 }
 
-criterion_group!(benches, bench_g1_addition);
+criterion_group!(benches, bench_bls12_381);
 
 criterion_main!(benches);
